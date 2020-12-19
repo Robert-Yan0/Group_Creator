1c3accc047645ce4d1b788cf9e98db1496866c94
Robert <robertyan64913@gmail.com>
2020-12-18 19:49:41 -0500

    added text box to enter names

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
