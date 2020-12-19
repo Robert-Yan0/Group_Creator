e05570e6799a82f0c6d2054e6e1b4f7d94e74c61
Robert <robertyan64913@gmail.com>
2020-12-19 14:35:16 -0500

    added number of groups
    this code will print out number of groups and people per group along side names

--- app.py
+++ app.py
@@ -6,11 +6,37 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     return render_template('home.html')
+ 
+#def parseNames(groupNames):
+#    #I will change this function later
+#     groupNames = groupNames.split()
+#     for x in groupNames:
+#         processedNames = processedNames + groupNames[x] + '\n'
+#     return processedNames
+
+def parseGroup(groupNames, groupNumber, groupPeople):
+    finalGroup = "Names:\n" + groupNames + "\n" + "Number of Groups:\n" + groupNumber + "\n" + "Number of people in a group:\n" + groupPeople
+    return finalGroup
 
 @app.route('/', methods=['POST'])
-def print_groups():
+def printNames():
     names = request.form['names']
-    return names
+    numOfGroups = request.form['numOfGroups']
+    peoplePerGroup = request.form['peoplePerGroup']
+    return parseGroup(names, numOfGroups, peoplePerGroup)
+
+#numOfGroups = request.form['numOfGroups']
+#peoplePerGroup = reqeust.form['peoplePerGroup']
+
+#@app.route('/', methods=['POST'])
+#def printGroupsNum():
+#    numOfGroups = request.form['numOfGroups']
+#    return numOfGroups
+
+#@app.route('/', methods=['POST'])
+#def printPeoplePerGroup():
+#    peoplePerGroup = request.form['peoplePerGroup']
+#    return peoplePerGroup
 
 
 @app.route("/about")
