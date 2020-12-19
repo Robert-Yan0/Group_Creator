6fafa385b89f073f0738dca2eab4eb59d483eada
Robert <robertyan64913@gmail.com>
2020-12-19 14:36:35 -0500

    removed arbitary comments

--- app.py
+++ app.py
@@ -6,13 +6,6 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     return render_template('home.html')
- 
-#def parseNames(groupNames):
-#    #I will change this function later
-#     groupNames = groupNames.split()
-#     for x in groupNames:
-#         processedNames = processedNames + groupNames[x] + '\n'
-#     return processedNames
 
 def parseGroup(groupNames, groupNumber, groupPeople):
     finalGroup = "Names:\n" + groupNames + "\n" + "Number of Groups:\n" + groupNumber + "\n" + "Number of people in a group:\n" + groupPeople
@@ -25,24 +18,9 @@ def printNames():
     peoplePerGroup = request.form['peoplePerGroup']
     return parseGroup(names, numOfGroups, peoplePerGroup)
 
-#numOfGroups = request.form['numOfGroups']
-#peoplePerGroup = reqeust.form['peoplePerGroup']
-
-#@app.route('/', methods=['POST'])
-#def printGroupsNum():
-#    numOfGroups = request.form['numOfGroups']
-#    return numOfGroups
-
-#@app.route('/', methods=['POST'])
-#def printPeoplePerGroup():
-#    peoplePerGroup = request.form['peoplePerGroup']
-#    return peoplePerGroup
-
-
 @app.route("/about")
 def about():
     return render_template('about.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
