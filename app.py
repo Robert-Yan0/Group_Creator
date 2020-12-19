2909fcac4c27ceaca79cdbd4be6a5a4fa148211d
Robert <robertyan64913@gmail.com>
2020-12-19 13:00:39 -0500

    updated code
    Code will now return names enter into the text box, will add number of groups and people per groups later

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+#I have no idea what i'm doing
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route('/', methods=['POST'])
+def print_groups():
+    names = request.form['names']
+    return names
+
 
 @app.route("/about")
 def about():
